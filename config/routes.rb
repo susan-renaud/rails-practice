b5b7e9c5d8a2de42e8bd9ba6abf0668a57f5194d
Susan Renaud <serenaud89@gmail.com>
2019-10-08 13:55:43 -0500

    deleted pages controller and associated files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'about', to: 'pages#about', as: :about
-  get 'contact', to: 'pages#contact', as: :contact
+
+
 end
