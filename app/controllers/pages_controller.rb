e8628a666f9e8c132c67d96e29939b0322142f34
Susan Renaud <serenaud89@gmail.com>
2019-10-31 10:50:57 -0500

    added contact page controller method to pages controlloer, created contact view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
 
   def projects
   end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 get 'projects', to: 'pages#projects'
-
+get 'contact', to: 'pages#contact'
 root to: 'pages#home'
 end
